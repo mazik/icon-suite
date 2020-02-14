79badf6da46832ceaa547b7b802043094dae3187
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2020-02-15 04:17:30 +0600

    Disable PWA only for Electron

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ module.exports = {
           args[0].title = "Icon Suite";
           return args;
         });
+        config.plugins.delete("workbox");
+        config.plugins.delete("pwd");
       },
       nodeIntegration: true
     }
