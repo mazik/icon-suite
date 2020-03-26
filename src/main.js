e6282f3983ad54ef6ca0aa525855e12204e4f6d1
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2020-03-26 19:46:56 +0600

    Add PrismJS for code highlighting

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import "prismjs";
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import VueClipboard from "vue-clipboard2";
+import "prismjs-material-theme/css/lighter.min.css";
+import "vue-prism-editor/dist/VuePrismEditor.css";
 
 Vue.use(VueClipboard);
 
